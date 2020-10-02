a1695867d44325fec3c1b58888b79ea344cb0fa8
tarcisio <tarc.car@gmail.com>
2020-10-01 21:43:11 -0300

    adicionado o input de ponto a melhorar

---
+++ client/src/components/PontoMelhorar/AutoComplete/index.tsx
@@ -0,0 +1,94 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
+import { useDispatch, useSelector } from 'react-redux';
+import { InputLabel } from '@material-ui/core';
+import { PontoMelhorar } from '../../../types/pontoMelhorar';
+import { getPontosMelhorarAction } from '../../../store/actions/pontoMelhorar/pontoMelhorarActions';
+import { RootState } from '../../../store/reducers';
+
+interface PontoMelhorarFilter extends PontoMelhorar{
+    inputValue?:string
+}
+const filter = createFilterOptions<PontoMelhorarFilter>();
+
+interface PontoMelhorarAutoCompleteProps {
+  onPontoMelhorarSelected:(v?:PontoMelhorar)=>void
+  error?:boolean
+}
+
+export const PontoMelhorarAutoComplete:React.FC<PontoMelhorarAutoCompleteProps> = (
+  { onPontoMelhorarSelected, error, ...props },
+) => {
+  const [value, setValue] = useState<PontoMelhorarFilter | null>(null);
+
+  const pontosMelhorar = useSelector(
+    (state:RootState) => state.pontoMelhorarReducer.pontosMelhorar,
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPontosMelhorarAction());
+  }, []);
+
+  useEffect(() => {
+    if (onPontoMelhorarSelected) onPontoMelhorarSelected(value as PontoMelhorar);
+  }, [value]);
+
+  return (
+    <>
+      <InputLabel>Pontos a melhorar</InputLabel>
+      <Autocomplete
+        {...props}
+        value={value}
+        onChange={(event, newValue: any) => {
+          if (typeof newValue === 'string') {
+            setValue({
+              descricao: newValue,
+            });
+          } else if (newValue && newValue.inputValue) {
+            setValue({
+              descricao: newValue.inputValue,
+            });
+          } else {
+            setValue(newValue);
+          }
+        }}
+        filterOptions={(options, params) => {
+          const filtered = filter(options as PontoMelhorarFilter[], params);
+
+          // Suggest the creation of a new value
+          if (params.inputValue !== '') {
+            filtered.push({
+              inputValue: params.inputValue,
+              descricao: `${params.inputValue}`,
+            });
+          }
+
+          return filtered;
+        }}
+        selectOnFocus
+        clearOnBlur
+        handleHomeEndKeys
+        id="free-solo-with-text-demo"
+        options={pontosMelhorar}
+        getOptionLabel={(option:any) => {
+          if (typeof option === 'string') {
+            return option;
+          }
+          if (option.inputValue) {
+            return option.inputValue;
+          }
+          return option.descricao;
+        }}
+        renderOption={(option:any) => option.descricao}
+        freeSolo
+        renderInput={(params) => (
+          <TextField {...params} fullWidth error={error} variant="outlined" />
+        )}
+      />
+    </>
+  );
+};
---
+++ client/src/components/PontoMelhorar/Input/index.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable no-unused-vars */
+import {
+  FormHelperText,
+  IconButton, List, ListItem, ListItemSecondaryAction, ListItemText,
+} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { PontoMelhorar } from '../../../types/pontoMelhorar';
+import { PontoMelhorarAutoComplete } from '../AutoComplete';
+
+interface InputPontoMelhorarProps{
+  errorText?: string,
+  fullWidth?:boolean,
+  onPontosMelhorarSelecionados:(v?:PontoMelhorar[])=>void
+}
+
+const InputPontoMelhorar: React.FC<InputPontoMelhorarProps> = (
+  {
+    onPontosMelhorarSelecionados, errorText, ...props
+  },
+) => {
+  const [pontosSelecionados, setPontosSelecionados] = useState<PontoMelhorar[]>([]);
+
+  const onChangeHandler = (value?:PontoMelhorar) => {
+    if (value) {
+      const novosPontos = [...pontosSelecionados, value];
+      setPontosSelecionados(novosPontos);
+    }
+  };
+
+  const onDeleteClickHandler = (index:number) => {
+    const novosPontos = [...pontosSelecionados];
+    novosPontos.splice(index, 1);
+    setPontosSelecionados(novosPontos);
+  };
+
+  useEffect(() => {
+    if (onPontosMelhorarSelecionados) onPontosMelhorarSelecionados(pontosSelecionados);
+  }, [pontosSelecionados]);
+
+  return (
+    <>
+      <PontoMelhorarAutoComplete onPontoMelhorarSelected={onChangeHandler} error={!!errorText} />
+      <List dense>
+        {pontosSelecionados.length > 0 && pontosSelecionados.map((p, index) => (
+          <ListItem key={`${p.descricao}_${index}`}>
+            <ListItemText
+              primary={p.descricao}
+            />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" onClick={() => { onDeleteClickHandler(index); }}>
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+
+      </List>
+      {errorText && (
+      <FormHelperText style={{
+        color: 'red',
+      }}
+      >
+        {errorText}
+      </FormHelperText>
+      )}
+    </>
+  );
+};
+
+export default InputPontoMelhorar;
