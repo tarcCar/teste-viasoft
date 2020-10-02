d7cd2d3e0033747f668ba8948df7f314d78ac97d
tarcisio <tarc.car@gmail.com>
2020-10-01 21:30:55 -0300

    adicionado pontoMelhorar reducer

--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import loginReducer from './loginReducer';
 import usuarioReducer from './usuarioReducer';
 import pontoManterReducer from './pontoManterReducer';
+import pontoMelhorarReducer from './pontoMelhorarReducer';
 
 const rootReducer = combineReducers({
   loginReducer,
   usuarioReducer,
   pontoManterReducer,
+  pontoMelhorarReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
